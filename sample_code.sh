26a4eb93e93f8d927a59adaa5ad1b8c309b853cb
devopsrini <69062045+devopsrini@users.noreply.github.com>
2020-08-03 19:15:33 -0400

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -5,6 +5,7 @@ echo "This line entered by srinivas"
 echo "This second line entered by srinivas"
 echo "This third line entered by srinivas"
 4th line
+5th line
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
